cabef0bb0833280fad26923b4ab8883b2f37717c
Wills Liou <willsliou@protonmail.com>
2019-09-01 21:55:03 -0700

    basic board

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,3 @@
 for (var i=0; i< 64; i++){
-    document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';    
+    document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';
 }
